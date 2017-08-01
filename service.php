7e3fc11c327e4d1b186b05fe26282b821ffd9c7c
salvipascual <salvi.pascual@gmail.com>
2017-08-01 18:28:05 -0400

    first commit

---
+++ service.php
@@ -0,0 +1,54 @@
+<?php
+
+use Goutte\Client;
+
+class Poema extends Service
+{
+	/**
+	 * Function executed when the service is called
+	 *
+	 * @param Request
+	 * @return Response
+	 * */
+	public function _main(Request $request)
+	{
+		// create a new client
+		$client = new Client();
+		$guzzle = $client->getClient();
+		$guzzle->setDefaultOption('verify', false);
+		$client->setClient($guzzle);
+
+		// create a crawler
+		$crawler = $client->request('GET', "http://poemacadadia.blogspot.com/");
+
+		// search for result
+		$title = $crawler->filter('.post-title')->text();
+		$poem = $crawler->filter('.post-body')->eq(0)->html();
+
+		// remove tildes
+		$poem = $this->utils->removeTildes($poem);
+
+		// clears the poem part
+		$dom = new DOMDocument;
+		@$dom->loadHTML($poem);
+		while (($r = $dom->getElementsByTagName("div")) && $r->length) {
+			$r->item(0)->parentNode->removeChild($r->item(0));
+		}
+		while (($r = $dom->getElementsByTagName("span")) && $r->length) {
+			$r->item(0)->parentNode->removeChild($r->item(0));
+		}
+		$poem = substr($dom->saveHTML(), 129, -15);
+
+		// create a json object to send to the template
+		$responseContent = array(
+			"title" => $title,
+			"poem" => $poem
+		);
+
+		// create the response
+		$response = new Response();
+		$response->setResponseSubject("El poema del dia");
+		$response->createFromTemplate("basic.tpl", $responseContent);
+		return $response;
+	}
+}
